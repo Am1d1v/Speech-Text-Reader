618f9d76baa36387e781545f07ff17273b9ea080
Am1d1v <dimadiman1adima@gmail.com>
2024-07-15 13:31:19 +0300

    Basic HTML markup. Array of data objects added

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+const main = document.querySelector('main');
+const voicesSelect = document.querySelector('#voices');
+const textarea = document.querySelector('#text');
+const readButton = document.querySelector('#read');
+const toggleButton = document.querySelector('#toggle');
+const closeButton = document.querySelector('#close');
+
+const data = [
+    {
+        image: 'https://images.unsplash.com/photo-1528669826296-dbd6f641707d?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        text: 'I am hungry'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1542013936693-884638332954?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        text: 'I am thirsty'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1479134262046-a470bfaf7a66?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        text: 'I am tired'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1604699229817-27301bdfed68?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        text: 'I am happy'
+    },
+];
+
+data.forEach(item => {
+    createBox(item);
+})
+
+// Create speech boxes
+function createBox(item){
+    const box = document.createElement('div');
+
+    // Destructuring every data array object
+    const {image, text} = item;
+
+    box.classList.add('box');
+    box.innerHTML = `
+        <img src="${image}" alt="${text}" />
+        <p>${text}</p>
+    `;
+}
